6604abc649b38d6e651e82731e46db6ec7ce752e
Mikhail Lyalin <mc.lyalin@gmail.com>
2022-03-22 20:03:47 +0500

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <HomePage />
     </div>
   );
 }
---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRODUCT GROUP 1</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRODUCT GROUP 2</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRODUCT GROUP 3</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRODUCT GROUP 4</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRODUCT GROUP 5</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
